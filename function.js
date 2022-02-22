b4a2e8d452ca1c9a56bfa7bcc3631dd869fe5ebb
Adrian Whitehurst <80975900+Adrianw90@users.noreply.github.com>
2022-02-22 17:24:00 -0500

    add_javafunction

---
+++ function.js
@@ -0,0 +1,9 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
